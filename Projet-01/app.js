f2a278126c479f540c8eeae944b8a1ee7c1e308c
Djibo Alhassane Ridwane <djiboridwanealhassane@gmail.com>
2024-12-25 10:24:58 +0100

    Projets SASS

---
+++ Projet-01/app.js
@@ -0,0 +1,6 @@
+document.getElementById('theme-bouton').addEventListener('click',function(){
+    document.body.classList.toggle('light-theme')
+
+    document.body.classList.toggle('dark-theme')
+
+})
